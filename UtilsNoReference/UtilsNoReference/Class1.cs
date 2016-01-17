0a094bc8a51c28eefbd32f3b19bd5c644f6fd9d6
Simone Invernizzi <simone.invernizzi@hotmail.com>
2016-01-17 21:16:33 +0100

    Furst Cinnut

---
+++ UtilsNoReference/UtilsNoReference/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UtilsNoReference
+{
+    public class Class1
+    {
+    }
+}
