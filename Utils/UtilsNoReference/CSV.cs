6829803e01555b681e15054319e4e63a44e4afde
Simone Invernizzi <simone.invernizzi@hotmail.com>
2016-01-17 21:26:51 +0100

    utils no reference

---
+++ Utils/UtilsNoReference/CSV.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UtilsNoReference
+{
+    public static class CSV
+    {
+        //rimuovo tutti i NUL presi da database/csv (/x00)
+        public static string ReplaceNull(string Raw)
+        {
+            return Regex.Replace(Raw, "\x00\x00", " ");
+        }
+
+
+        public static string ToCSV_String(DataSet dataset)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            //Header
+            for (int k = 0; k < dataset.Tables[0].Columns.Count - 1; k++)
+                sb.Append(dataset.Tables[0].Columns[k].ColumnName.ToLower() + ';'); //se un csv inizia per ID verrà letto come SYLK
+            sb.Append(dataset.Tables[0].Columns[dataset.Tables[0].Columns.Count - 1].ColumnName.ToLower()); //ultimo elemento senza ;
+
+            //append new line
+            sb.Append("\r\n");
+            for (int i = 0; i < dataset.Tables[0].Rows.Count; i++)
+            {
+                //add separators
+                for (int k = 0; k < dataset.Tables[0].Columns.Count; k++)
+                {
+                    string aggiungi_colonna = dataset.Tables[0].Rows[i].ItemArray[k].ToString();
+
+                    if (k == dataset.Tables[0].Columns.Count - 1) //ultima colonna senza ;
+                        sb.Append(aggiungi_colonna);
+                    else
+                    {
+                        sb.Append(aggiungi_colonna + ';');
+                    }
+                }
+
+                //append new line
+                sb.Append("\r\n");
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ Utils/UtilsNoReference/LOG_FILE.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UtilsNoReference
+{
+    //todo: async con FileIO
+    public static class LOG_FILE
+    {
+        private static readonly object threadLock = new object();
+
+        public static void WriteLine(string filename, string msg)
+        {
+            lock (threadLock)
+            {
+                string log_path = AppDomain.CurrentDomain.BaseDirectory + filename + ".txt";
+                string new_line = DateTime.Now.ToString("dd/MM/yyyy hh:mm:ss") + " " + msg + Environment.NewLine + Environment.NewLine;
+                File.AppendAllText(log_path, new_line);
+            }
+        }
+        public static void WriteLine(string msg)
+        {
+            lock (threadLock)
+            {
+                string log_path = AppDomain.CurrentDomain.BaseDirectory + "Log_" + DateTime.Now.Day + "_" + DateTime.Now.Month + "_" + DateTime.Now.Year + ".txt";
+                string new_line = DateTime.Now.ToString("dd/MM/yyyy hh:mm:ss") + " " + msg + Environment.NewLine + Environment.NewLine;
+                File.AppendAllText(log_path, new_line);
+            }
+        }
+
+        public static void Write(Exception Exception, string TitoloEccezione)
+        {
+            string testo = DateTime.Now.ToLongTimeString() + Environment.NewLine +
+                           TitoloEccezione + Environment.NewLine +
+                           Exception.ToString() + Environment.NewLine + Environment.NewLine;
+
+
+            string nomeFile = "Log_" + DateTime.Now.ToString("dd_MM_yyyy") + ".txt";
+            string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, nomeFile);
+
+            lock (threadLock)
+            {
+                if (!File.Exists(path))
+                {
+                    File.WriteAllText(path, testo);
+                }
+                else
+                {
+                    File.AppendAllText(path, testo);
+                }
+            }
+        }
+    }
+}
