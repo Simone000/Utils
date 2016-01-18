4fe4530b27d795e6680188b99440c4bf88fefb9c
Simone Invernizzi <simone.invernizzi@hotmail.com>
2016-01-18 11:23:20 +0100

    cryptography

---
+++ Utils/UtilsCryptography/CRYPTOGRAPHY.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UtilsCryptography
+{
+    public static class CRYPTOGRAPHY
+    {
+        /*
+         * -SHA1: recommended for the best encryption strength.
+         * -MD5 (Message Digest 5): better performance
+         * AES (Rijndael)
+         * 3DES (TripleDES): applica tre volte consecutive il DES (Data Encryption Standard).
+        */
+
+
+
+        //usabile per creare la MachineKey se lunga 40-128 caratteri
+        public static string CreateKey_Hexadecimal(int length)
+        {
+            byte[] random_bytes = new byte[length / 2]; //ogni carattere usa 2 byte
+
+            // Create a cryptographically strong random number generator and fill the byte array
+            RNGCryptoServiceProvider crypt_rng = new RNGCryptoServiceProvider();
+            crypt_rng.GetBytes(random_bytes);
+
+            //converto in esadecimale
+            return BitConverter.ToString(random_bytes).Replace("-", string.Empty);
+        }
+    }
+}
