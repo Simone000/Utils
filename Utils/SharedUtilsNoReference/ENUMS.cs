beb25c4afdef5aeffbcc77b5b54c015032d307bb
Simone Invernizzi <simone.invernizzi@hotmail.com>
2016-02-24 14:14:07 +0100

    enum to string utils

---
+++ Utils/SharedUtilsNoReference/ENUMS.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharedUtilsNoReference
+{
+    public static class ENUMS
+    {
+        public static String GetName(this Enum eff)
+        {
+            return Enum.GetName(eff.GetType(), eff);
+        }
+    }
+}
