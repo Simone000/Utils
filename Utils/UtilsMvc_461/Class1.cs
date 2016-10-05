e5bd44da87aa5d2c14dc854ed4b572b8daf69d09
Simone Invernizzi <simone.invernizzi@hotmail.com>
2016-10-05 16:24:00 +0200

    4.6.2 version

---
+++ Utils/UtilsMvc_461/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UtilsMvc_461
+{
+    public class Class1
+    {
+    }
+}
