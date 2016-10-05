145540143215c15a7e8f3b739abe5fee141e0d8f
Simone Invernizzi <simone.invernizzi@hotmail.com>
2016-10-05 21:06:57 +0200

    custom validation exc

---
+++ Utils/SharedUtilsNoReference/Exceptions/CustomValidationException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharedUtilsNoReference.Exceptions
+{
+    /// <summary>
+    /// To return BadRequest(message) from library outside webapi
+    /// </summary>
+    [Serializable]
+    public class CustomValidationException : Exception
+    {
+        public CustomValidationException() { }
+        public CustomValidationException(string message) : base(message) { }
+        public CustomValidationException(string message, Exception inner) : base(message, inner) { }
+
+        protected CustomValidationException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+}
