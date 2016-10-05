308f65ec9cb82ce6245bff0997989d7132d5e935
Simone Invernizzi <simone.invernizzi@hotmail.com>
2016-10-05 22:14:29 +0200

    new version razor render

--- Utils/SharedUtilsMvc/Rendering.cs
+++ Utils/SharedUtilsMvc/Rendering.cs
@@ -4,7 +4,9 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 using System.Web.Mvc;
+using System.Web.Routing;
 
 namespace SharedUtilsMvc
 {
@@ -29,5 +31,31 @@ namespace SharedUtilsMvc
                 return sw.ToString();
             }
         }
+
+        /// <summary>
+        /// var html = TempUtils.RenderViewToString("Templates", "FatturaTemplate2", templateModel);
+        /// </summary>
+        /// <param name="controllerName"></param>
+        /// <param name="viewName"></param>
+        /// <param name="viewData"></param>
+        /// <returns></returns>
+        public static string RenderViewToString(string controllerName, string viewName, object viewData)
+        {
+            using (var writer = new StringWriter())
+            {
+                var routeData = new RouteData();
+                routeData.Values.Add("controller", controllerName);
+                var fakeControllerContext = new ControllerContext(new HttpContextWrapper(new HttpContext(new HttpRequest(null, "http://google.com", null), new HttpResponse(null))), routeData, new FakeController());
+                var razorViewEngine = new RazorViewEngine();
+                var razorViewResult = razorViewEngine.FindView(fakeControllerContext, viewName, "", false);
+
+                fakeControllerContext.Controller.ControllerContext = fakeControllerContext;
+                var viewContext = new ViewContext(fakeControllerContext, razorViewResult.View, new ViewDataDictionary(viewData), new TempDataDictionary(), writer);
+                razorViewResult.View.Render(viewContext, writer);
+                return writer.ToString();
+            }
+        }
+
+        private class FakeController : ControllerBase { protected override void ExecuteCore() { } }
     }
 }
