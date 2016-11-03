759e5c62b52cf34d6f293b9d0e1b86b2125b786f
Simone Invernizzi <simone.invernizzi@hotmail.com>
2016-11-03 14:26:48 +0100

    xml utils

---
+++ Utils/SharedUtilsNoReference/XML.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace SharedUtilsNoReference
+{
+    public static class XML
+    {
+        public static string SerializeToXml<T>(T value)
+        {
+            string ris = string.Empty;
+            using (StringWriter writer = new StringWriter(CultureInfo.InvariantCulture))
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(T));
+                serializer.Serialize(writer, value);
+                ris = writer.ToString();
+            }
+            return ris;
+        }
+
+        public static T DeserializeFromXml<T>(string value)
+        {
+            T ris;
+            XmlSerializer serializer = new XmlSerializer(typeof(T));
+            using (var reader = new StringReader(value))
+            {
+                ris = (T)serializer.Deserialize(reader);
+            }
+            return ris;
+        }
+    }
+}
