01fae38ac63e204bbb28f301f3c48a24fe1a31ee
simone <simone.invernizzi@hotmail.com>
2017-03-06 17:26:27 +0100

    unit testing

---
+++ Utils/TestNoReference/TestTASKS.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SharedUtilsNoReference;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestNoReference
+{
+    [TestClass]
+    public class TestTASKS
+    {
+        private async Task<int> NotImplementedExceptionMethodAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NotImplementedException))]
+        public async Task TestRetryOnFaultAsync()
+        {
+            await TASKS.RetryOnFaultAsync(() => NotImplementedExceptionMethodAsync());
+        }
+    }
+}
